149ded06be00b5b2b6088621c9fb0be96fbead2c
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-23 21:47:32 +0200

    adding summaries and problem solving pages

--- app/problem-solving/page.tsx
+++ app/problem-solving/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
 export default function ProblemSolvingPage() {
   return (
-    <div>ProblemSolvingPage</div>
-  )
+    <div className="min-h-[90vh] grid place-items-center ">
+      <h1 className="font-playfair text-6xl">Coming Soon</h1>
+    </div>
+  );
 }
--- app/summaries/page.tsx
+++ app/summaries/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function SummariesPage() {
   return (
-    <div>SummariesPage</div>
+    <div className="min-h-[90vh] grid place-items-center ">
+      <h1 className="font-playfair text-6xl">Coming Soon</h1>
+    </div>
   )
 }

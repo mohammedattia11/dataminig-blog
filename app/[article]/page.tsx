a149a804e41381d40422b2de8bfe8b0543bd01a8
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-24 13:35:54 +0200

    fix:fixing style issues and chapter-2 indentation problem

--- app/[article]/page.tsx
+++ app/[article]/page.tsx
@@ -1,13 +1,17 @@
-import { getArticleData } from "@/lib/articles"
-import Link from "next/link"
+import { getArticleData } from "@/lib/articles";
 
 type Props = {
-  params : Promise<{article:string}>
-}
-export default async function ArticlePage({params}:Props) {
-  const {article} = await params;
-  const articleData = await getArticleData(article)
-  return <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
-    <article className="article" dangerouslySetInnerHTML={{__html:articleData.htmlContent}}/>
-  </section>
+  params: Promise<{ article: string }>;
+};
+export default async function ArticlePage({ params }: Props) {
+  const { article } = await params;
+  const articleData = await getArticleData(article);
+  return (
+    <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
+      <article
+        className="article"
+        dangerouslySetInnerHTML={{ __html: articleData.htmlContent }}
+      />
+    </section>
+  );
 }

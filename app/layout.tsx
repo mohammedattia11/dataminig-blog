9572f00d8fda0d916f62fe20aba60b30917bd026
mohamed atia <mohamedd.attia11@gmail.com>
2026-01-11 00:02:03 +0200

    fix:forcing dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,8 +52,9 @@ export default function RootLayout({
             <ThemeProvider
               attribute="class"
               defaultTheme="dark"
-              enableSystem
+              enableSystem={false}
               disableTransitionOnChange
+              forcedTheme="dark"
             >
               <TubeLightNavbar />
               {children}
